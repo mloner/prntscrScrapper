3f86dcb46831154676c2175b842a6115f02237cc
Nikita Romashov <shipshon@gmail.com>
2020-02-17 23:05:57 +0400

    bugs fixed

--- main.py
+++ main.py
@@ -1,19 +1,33 @@
-import string, random, os, sys, _thread, httplib2, time
-
+import string, random, os, sys, _thread, httplib2, shutil
 
 if len(sys.argv) < 2:
     sys.exit("\033[37mUsage: python3 " + sys.argv[0] + " (Number of threads)")
-THREAD_AMOUNT = int(sys.argv[1])
 
+THREAD_AMOUNT = int(sys.argv[1])
 INVALID = [0, 503, 5082, 4939, 4940, 4941, 12003, 5556]
-countOfPictures = 0
+picsInThread = []
+currentPicsInThread = []
+done = False
+count = int(sys.argv[2]) // int(sys.argv[1])
+
+for i in range(int(sys.argv[1])):
+    currentPicsInThread.append(0)
+
+for i in range(int(sys.argv[1])):
+    picsInThread.append(count)
+
+if int(sys.argv[2]) % int(sys.argv[1]) != 0:
+    last = int(sys.argv[2]) - int(sys.argv[1]) * count
+    picsInThread[int(sys.argv[1]) - 1] += last
+
 def scrape_pictures(thread):
-    #print(sys.argv[2])
-    global countOfPictures
+    global done
     while True:
-        if len(sys.argv) > 2 and countOfPictures >= int(sys.argv[2]):
-            # print('done' + str(_thread.get_ident()))
-            #print('done thread')
+        if len(sys.argv) > 2 and int(currentPicsInThread[int(thread)-1]) >= int(picsInThread[int(thread)-1]):
+            print("thread " + thread + " done")
+            if currentPicsInThread == picsInThread:
+                print("All threads are done")
+                done = True
             sys.exit()
         url = 'http://i.imgur.com/'
         length = random.choice((5, 6))
@@ -23,11 +37,7 @@ def scrape_pictures(thread):
             url += ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(3))
             url += ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(3))
         url += '.jpg'
-        # print(url)
-
         filename = url.rsplit('/', 1)[-1]
-        # print (filename)
-
         h = httplib2.Http('.cache' + thread)
         response, content = h.request(url)
         response = h.request(url)[0]
@@ -35,14 +45,13 @@ def scrape_pictures(thread):
             out = open('pcs/' + filename, 'wb')
             out.write(content)
             out.close()
-
             file_size = os.path.getsize('pcs/' + filename)
             if file_size in INVALID:
-                #print(str(response.status) + "[-] Invalid: " + url)
                 os.remove('pcs/' + filename)
             else:
-                countOfPictures += 1
-                #print(str(response.status) + " [+] Valid: " + url)
+                currentPicsInThread[int(thread) - 1] += 1
+            path = os.path.join(os.path.abspath(os.path.dirname(__file__)), '.cache' + thread)
+            shutil.rmtree(path)
 
 for thread in range(1, THREAD_AMOUNT + 1):
     thread = str(thread)
@@ -52,5 +61,5 @@ for thread in range(1, THREAD_AMOUNT + 1):
         print('Error starting thread ' + thread)
 print('Succesfully started ' + thread + ' threads.')
 
-while True:
-    time.sleep(1)
+while not done:
+    pass
